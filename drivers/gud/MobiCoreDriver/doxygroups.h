cc8005aef032816fd6be6e42d4284e0673ee1e15
Valentin Manea <valentin.manea@trustonic.com>
2014-04-06 18:25:53 +0300

    Add MobiCore platform support to Goldfish
    Add all the needed MobiCore driver files to the kernel tree

---
+++ drivers/gud/MobiCoreDriver/doxygroups.h
@@ -0,0 +1,31 @@
+/*
+ * DoxyGroups.h
+ */
+
+#ifndef DOXYGROUPS_H_
+#define DOXYGROUPS_H_
+
+/*
+ * @defgroup MCD                        MobiCore Driver
+ *
+ * @defgroup MCD_API                    MobiCore Driver API
+ * @ingroup  MCD
+ *
+ * @defgroup MCD_IMPL_LIB               Client Library
+ * @ingroup  MCD
+ *
+ * @defgroup MCD_MCDIMPL_DAEMON         Daemon
+ * @ingroup  MCD
+ *
+ * @defgroup MCD_MCDIMPL_KMOD           Kernel Driver
+ * @ingroup  MCD
+ *
+ * @defgroup MCD_MCDIMPL_KMOD_IMPL      Kernel Driver Implementation
+ * @ingroup  MCD_MCDIMPL_KMOD
+ *
+ * @defgroup MCD_MCDIMPL_KMODAPI        Kernel API
+ * @ingroup  MCD
+ *
+ */
+
+#endif /* DOXYGROUPS_H_ */
